5f334f161a1870fc8956beab613f2ad363eaad1e
Albert Li <ali@lvivsoft.com>
2017-04-19 17:51:28 +0300

    display jobs posted during last 30min

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from datetime import datetime
-import base64
-import hashlib
+import datetime
 import upwork
 import json
 from time import sleep
@@ -8,8 +6,13 @@ import os
 import webbrowser
 
 
-def encode(ele):
-    return base64.b64encode(hashlib.sha1(str(ele)).digest())
+def play_and_print(job):
+    os.system('aplay {}'.format(NOTIFICATION_FILE))
+    print 'Time : {}\nJob Title : {}\nURL : {}\n'.format(
+        datetime.datetime.now().strftime('%H:%M'),
+        job['title'], job['url']
+    )
+    webbrowser.open(url=job['url'], autoraise=True, new=2)
 
 
 def get_client():
@@ -46,23 +49,42 @@ if __name__ == '__main__':
 
     all_ids = []
 
-    queries = ['python', 'django']
+    queries = ['python', 'django', 'web scraping']
     initial_jobs = []
 
     for q in queries:
         initial_jobs.extend(client.provider_v2.search_jobs({'q': q}))
 
+    # show jobs posted during last 30 min
+    current_time = datetime.datetime.utcnow().replace(microsecond=0)
+    print "\nJobs posted during last 30 min\n".upper()
+    counter = 1
+    recent_jobs = []
+    for job in initial_jobs:
+        date_created = job['date_created'].replace('T', ' ').replace("+0000", '')
+        job_posted = datetime.datetime.strptime(date_created, "%Y-%m-%d %H:%M:%S")
+        if job_posted + datetime.timedelta(minutes=30) > current_time and job['id'] not in recent_jobs:
+            recent_jobs.append(job['id'])
+            print counter, "TITLE:", job['title']
+            print " ", " LINK:", job['url']
+            print " Posted: {0} minutes ago".format(str(current_time-job_posted)[2:4])
+            counter += 1
+            print "-----------------------"
+
     initial_ids = [job['id'] for job in initial_jobs]
     all_ids.extend(initial_ids)
-    print "Lets start to search new tasks \n"
+    print "Lets start to search new tasks for: {0}\n".format([q for q in queries])
 
     while True:
         # Get all latest jobs
         print '\nGetting Jobs at {} ...\n'.format(
-            datetime.now().strftime('%H:%M'))
+            datetime.datetime.now().strftime('%H:%M'))
         jobs = []
         for q in queries:
-            jobs.extend(client.provider_v2.search_jobs({'q': q}))
+            try:
+                jobs.extend(client.provider_v2.search_jobs({'q': q}))
+            except Exception:
+                continue
 
         # Iterate every job
         for job in jobs:
@@ -70,11 +92,5 @@ if __name__ == '__main__':
             # Check if not viewed; new job
             if job['id'] not in all_ids:
                 all_ids.append(job['id'])
-                os.system('aplay {}'.format(NOTIFICATION_FILE))
-                print 'Time : {}\nJob Title : {}\nURL : {}\n'.format(
-                    datetime.now().strftime('%H:%M'),
-                    job['title'], job['url']
-                )
-                webbrowser.open(url=job['url'], autoraise=True, new=2)
         # 1.5 minutes rest
         sleep(90)
